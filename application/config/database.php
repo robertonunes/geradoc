295026f8165a5e7fb18517d5650df5e574cbe04b
Tarso de Castro <tarsodecastro@gmail.com>
2014-08-07 22:24:05 -0300

    usuário admin

--- application/config/database.php
+++ application/config/database.php
@@ -67,9 +67,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = 'geradoc';
+$db['default']['username'] = 'root';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'gerador_zerado';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
