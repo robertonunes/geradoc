22842b425719c850cc9f3865860eb20e597d2dca
Tarso de Castro <tarsodecastro@gmail.com>
2014-08-11 20:53:36 -0300

    database

--- application/config/database.php
+++ application/config/database.php
@@ -35,7 +35,7 @@
 | make active.  By default there is only one group (the 'default' group).
 |
 | The $active_record variables lets you determine whether or not to load
-| the active record class
+| the active record class 
 */
 
 /* DEFAULT */
