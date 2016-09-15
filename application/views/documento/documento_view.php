dc19fcbe9fa1a90125016d9c50dceb83bf896ad3
tarso <tarsodecastro@gmail.com>
2016-09-15 09:30:20 -0300

    views

--- application/views/documento/documento_view.php
+++ application/views/documento/documento_view.php
@@ -1,16 +1,12 @@
 <link rel="stylesheet" href="<?php echo base_url(); ?>css/pdf.css">
-<link href="<?php echo base_url(); ?>js/jquery-ui.min.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="<?php echo base_url(); ?>bootstrap/css/bootstrap-select.min.css">
-
-<script type="text/javascript" src="<?php echo base_url(); ?>js/datepicker/js/jquery.ui.datepicker-pt-BR.js"></script>
-<script src="<?php echo base_url(); ?>js/jquery-ui.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.maskedinput.min.js"></script>
 
 <div class="areaimage">
 	<center>
-		<h4 class="text-mutted"><img src="{TPL_images}Actions-document-edit-icon.png" height="62px" /> <?php echo $titulo;?></h4>
-	</cente>
+		<img src="{TPL_images}Actions-document-edit-icon.png" height="72px" />
+	</center>
 </div>
+
+<p class="bg-success lead text-center">Documento</p>
 
 <div id="msg" style="display: none;">
 	<img src="{TPL_images}loader.gif" alt="Enviando" />Aguarde
@@ -31,8 +27,6 @@
 			    echo $link_workflow;
 			    
 			    if ($carimbos == 'yes'){
-
-			    $texto_campoDataAlerta = ($campoDataAlerta != '') ? ': <strong class="text-danger">' . $campoDataAlerta . ' - ' . $campoHoraAlerta.'</strong>' : '';			
 		    ?>
 		    <div class="btn-group">
 			  <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
@@ -45,9 +39,7 @@
 			    <li><?php echo $carimbo_urgente;?></li>
 			  </ul>
 			</div> 
-			<a href="#" class="btn btn-default btn-sm" data-original-title="" title="" data-toggle="modal" data-target="#modalAlerta"><i class="cus-bell_add"></i> Alerta<?php echo $texto_campoDataAlerta;?></a>
 			<?php }?>
-
 		  	</div> 
 		  	
 	    </div>
@@ -162,101 +154,3 @@
 	
 </div>
 <!-- fim da div  view_content -->
-
-
-<!-- Modal -->
-<div class="modal fade" id="modalAlerta" tabindex="-1" role="dialog" aria-labelledby="Alerta">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h3 class="modal-title" id="myModalLabel"><i class="fa fa-bell-o fa-lg" style="color: #f0ad4e;"></i> Adicionar alerta</h3>
-      </div>
-      
-      <form  id="contactForm" role="form" action="<?php echo $form_action_alerta;?>" method="post">
-   
-			<div class="modal-body">
-			
-					<?php if($mostraFormAddAlerta == true){?>
-					<div class="alert alert-warning text-justify" role="alert" style="padding: 10px;">
-						<h4><strong>Leia-me!</strong></h4>
-						<p class="text-justify"> Para adicionar um <strong>novo</strong> alerta para <strong>este documento</strong>, preencha os cambos abaixo:</p>
-					</div>
-				
-			      	  <div class="row">
-			      	  
-			      	  	<div class="col-md-6">
-					  	  	<div class="form-group">
-				            	<label for="campoDataAlerta" class="control-label">Data:</label>
-				            	<input type="text" name="campoDataAlerta" id="campoDataAlerta" value="" placeholder="Informe a data" maxlength="10" size="12" class="form-control" required style="width: 150px;">
-				          	</div>
-				         </div>
-				          
-				         <div class="col-md-3">
-				          	<div class="form-group">
-				           	 	<label for="campoHoraAlerta" class="control-label">Hora:</label>
-				           	 	<input type="text" name="campoHoraMinutoAlerta" id="campoHoraMinutoAlerta" value="" placeholder="Informe a hora" maxlength="5" class="form-control text-left" required>
-				          	</div>
-				         </div>
-				        
-			          </div>
-
-					  <div class="row">
-					  	<div class="col-md-12">
-				          <div class="form-group">
-						  		<label for="campoMotivoAlerta" class="control-label">Motivo:</label>
-							    <textarea class="form-control" rows="2" name="campoMotivoAlerta" id="campoMotivoAlerta" placeholder="Motivo" required></textarea>
-						  </div>
-						 </div>
-					  </div>
-					  
-					  <?php }else{?>
-						
-						<div class="alert alert-danger text-justify" role="alert" style="padding: 10px;">
-							<h4><strong>Leia-me!</strong></h4>
-							<h4 class="text-center"> Já existe um alerta cadastrado para esse documento.</h4>
-							<h4 class="text-center"> Data: <strong><?php echo $campoDataAlerta; ?></strong>, hora: <strong><?php echo $campoHoraAlerta;?></strong></h>
-							<p></p>
-							<p class="text-center"> <a href="<?php echo site_url('/alerta'); ?>" class="btn btn-default btn-sm"><i class="cus-bell"></i> Clique aqui para verificar seus alertas</a></p>
-						</div>
-						
-					 <?php }?>
-	  	       	
-			</div>
-			      
-		      <div class="modal-footer">
-		      
-			        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-			        
-			        <?php 
-						if($campoDataAlerta == ''){
-					?>
-			        <button type="submit" class="btn btn-warning">Salvar <span class="glyphicon glyphicon glyphicon-ok"></span></button>
-			        
-			        <?php }?>
-		        
-		      </div>
-			      
-		</form>
-
-
-    </div>
-  </div>
-</div>
-<script type="text/javascript">
-
-$(document).ready(function(){
-
-	$.datepicker.setDefaults( $.datepicker.regional[ "pt-BR" ] );
-	$( "#campoDataAlerta" ).datepicker({
-		beforeShow: function() {
-	        setTimeout(function(){
-	            $('.ui-datepicker').css('z-index', 99999999999999);
-	        }, 0);
-	    }
-	});
-
-	$("#campoHoraMinutoAlerta").mask("99:99");
-
-});
-</script>
